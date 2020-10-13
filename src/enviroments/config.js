3c7425d4bc21924cb6e8a98a0b98d62adbf7eb5a
The Noy <the.noy.art@gmail.com>
2020-10-13 18:49:00 +0200

    some fixes - probablu

--- src/enviroments/config.js
+++ src/enviroments/config.js
@@ -1,3 +1,3 @@
 export const localApi = 'http://localhost:3000';
-export const imageApi = 'http://vps-75457e71.vps.ovh.net';
-export const api = `${imageApi}:8029`;
+export const imageApi = 'http://ec2-54-75-73-234.eu-west-1.compute.amazonaws.com';
+export const api = `${imageApi}:8020`;
